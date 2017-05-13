6f2514aa4d7d5d3864b37929bc7cc925b03603e2
Matthew Dickson <mattd342@gmail.com>
2017-05-13 23:40:37 +0100

    Changed to fish, new configs

---
+++ .config/fish/functions/config.fish
@@ -0,0 +1,3 @@
+function config --description 'alias config /usr/bin/git --git-dir=/home/matt/.dotfiles/ --work-tree=/home/matt'
+	/usr/bin/git --git-dir=/home/matt/.dotfiles/ --work-tree=/home/matt $argv;
+end
