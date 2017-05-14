bbbf711160ad117e9bdc907317488dc525d06d0c
Matthew Dickson <mattd342@gmail.com>
2017-05-14 18:54:28 +0100

    Fish was cool, but it kept swimming away when I ran make. Back to zsh pretending to be fish...

--- .config/fish/functions/config.fish
+++
@@ -1,3 +0,0 @@
-function config --description 'alias config /usr/bin/git --git-dir=/home/matt/.dotfiles/ --work-tree=/home/matt'
-	/usr/bin/git --git-dir=/home/matt/.dotfiles/ --work-tree=/home/matt $argv;
-end
--- .config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,4 +0,0 @@
-function fish_user_key_bindings
-  fish_hybrid_key_bindings
-  bind -M insert -m default jk backward-char force-repaint
-end 
