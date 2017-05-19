a1bc00dad405c71640b80751853d2f8b2a4abb7d
Matt <mattd342@gmail.com>
2017-05-19 20:25:43 +0100

    Switched to KDE, no need for the old guys :)

--- bin/screensetup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-builtin=LVDS-1	
-external=DP-1
-
-if xrandr | grep "$external disconnected"; then
-	xrandr --output "$external" --off --output "$builtin" --auto
-else
-	xrandr --output "$builtin" --off --output "$external" --auto
-fi
